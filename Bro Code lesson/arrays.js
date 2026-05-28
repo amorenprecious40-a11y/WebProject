034cfaba3d703122b7de92b792bb042188d0cb5f
amorenprecious40-a11y <amorenprecious40@gmail.com>
2026-05-28 08:21:14 -0700

    Concluded arrays

--- Bro Code lesson/arrays.js
+++ Bro Code lesson/arrays.js
@@ -5,10 +5,18 @@ let fruits = ["apple", "orange", "banana"];
 
 
 
-// fruits.push("coconut");
-// fruits.pop();
+// fruits.push("coconut"); // to add an element to the end of an array
+// fruits.pop(); // to remove the last element of an array
+// fruits.unshift("mango"); //to add element to the beginning of an array
+// fruits.shift(); // to remove an element from the beginning of an array
 
-console.log(fruits[0]);
-console.log(fruits[1]);
-console.log(fruits[2]);
-console.log(fruits[3]);
+
+// to get the length of an array
+
+
+for (let i = 0; i < fruits.length; i++) {
+    const element = fruits[i];
+    console.log(element)
+    
+}
+
