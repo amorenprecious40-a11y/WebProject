57622d4e49f698710d4cd59bd20dad62be464470
amorenprecious40-a11y <amorenprecious40@gmail.com>
2026-05-27 21:07:47 -0700

    Learning arrays in javascript

---
+++ Bro Code lesson/arrays.js
@@ -0,0 +1,14 @@
+// array = a variable like structure that can hold more than one value
+
+
+let fruits = ["apple", "orange", "banana"];
+
+
+
+// fruits.push("coconut");
+// fruits.pop();
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
